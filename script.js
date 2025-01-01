a00068218959d913fbdd20c790832a7c28bfb916
Md Jawwad Bin Kaysar Refat <rifat732151@gmail.com>
2025-01-01 22:46:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["PORTFOLIO", "MD REFAT"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+  }
+    )
+
+var typed = new Typed(".multiple-text2", {
+    strings: ["Frontend Developer", "YouTuber", " Blogger"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+  }
+    )
+
+var typed = new Typed(".multiple-text3", {
+    strings: ["Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam vel quasi voluptatibus quos. Unde necessitatibus non ab,in velit odio reiciendis  fugiat dolores ratione accusamus! Amet, nisi."],
+    typeSpeed: 50,
+  }
+    )
+
+
+
+
+
+
+
+
